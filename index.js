56ebf05b8cda7294b04e2fdc6f7e353b4186ea5a
puppy0cam <7194673+puppy0cam@users.noreply.github.com>
2020-06-15 10:47:01 +1000

    column spec

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import Visualization from 'zeppelin-vis';
 import PassthroughTransformation from 'zeppelin-tabledata/passthrough';
+import ColumnselectorTransformation from 'zeppelin-tabledata/columnselector'
 
 export default class ZeppelinOpenLayers extends Visualization {
     constructor(targetEl, config) {
         super(targetEl, config);
         this.passthrough = new PassthroughTransformation(config);
+
+        const columnSpec = [
+          { name: 'layer url' },
+          { name: 'layer name'},
+        ];
+    
+        this.transformation = new ColumnselectorTransformation(config, columnSpec);
+        console.log(this, targetEl, config);
     }
 
     render(tableData) {
